89228a28cce499c4a692b3e5fed6a3bea78f8d85
oozen <ozgrozn79@gmail.com>
2017-04-27 16:57:10 +0300

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule}   from '@angular/forms'; // <-- NgModel lives here
+
+import {AppComponent} from './components/app.component';
+
+
+@NgModule({
+    imports: [BrowserModule, FormsModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
